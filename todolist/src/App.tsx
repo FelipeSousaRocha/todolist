a0079a9407289e6161de3346c7413f2ca3ca0155
felipedsousarocha <felipedsousarocha@gmail.com>
2023-02-12 19:55:14 -0300

    Parte inicial do sistema feita

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,11 +1,13 @@
-import './index.css'
-
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-black text-white min-h-screen">
+      <div className="m-auto max-w-4xl p-2.5">
+        <h1 className="m-0 p-0 text-white text-center border-b border-solid border-purple-neon pb-5 font-bold text-3xl">
+        Lista de Tarefas
+        </h1>
+      </div>
+    </div>
   )
 }
 
--- todolist/tailwind.config.cjs
+++ todolist/tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'purple-neon': '#993399',
+      black: 'black',
+      white: 'white'
+    },
     extend: {}
   },
   plugins: []
